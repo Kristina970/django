ab37f1e9857a39ad4076834f06110bb4b2ec44d2
Kristina970 <khrystyna.mashchakevych@gmail.com>
2019-06-23 13:28:24 +0300

    settings.py changed staticfile storage

--- myblog/settings.py
+++ myblog/settings.py
@@ -124,4 +124,4 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
